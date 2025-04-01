f83c0e06d3ea8f85a3b6e28a0f3d6b5a9f8d710b
yashbim <ybimsara03@gmail.com>
2025-04-02 01:04:59 +0530

    Added sample images for view

--- src/components/ViewDesigns.jsx
+++ src/components/ViewDesigns.jsx
@@ -2,9 +2,7 @@
 import Navbar from "./Navbar";
 import { useNavigate } from "react-router-dom";
 
-
 const ViewDesigns = () => {
-
 
     const navigate = useNavigate();
     const handleNewDesign = () =>{
@@ -12,14 +10,18 @@ const ViewDesigns = () => {
     }
 
   return (
-<div className="min-h-screen bg-cover bg-center">
-    
+    <div className="min-h-screen bg-cover bg-center">
       <Navbar />
       <title>View Designs</title>
       <div className="flex flex-col md:flex-row items-center justify-center p-10 gap-8">
         {/* Left Section*/}
-        <div className={`md:w-3/4 border-3 p-16 flex flex-col items-center justify-center text-gray-600 cursor-pointer bg-white rounded-lg h-96 transition-all `}>
-            {/* image grid */}
+        <div className={`md:w-3/4 border-3 p-16 flex items-center justify-center text-gray-600 cursor-pointer bg-white rounded-lg h-96 transition-all `}>
+          {/* image grid */}
+          <div className="grid grid-cols-3 gap-4 w-full h-full">
+            <img src="src\assets\samples\2f2ba762c9db4434b7df1ce426e0758c.jpg" alt="Design 1" className="object-cover w-full h-full rounded-lg" />
+            <img src="src\assets\samples\05ca058f615842878768b567bc24271d.jpg" alt="Design 2" className="object-cover w-full h-full rounded-lg" />
+            <img src="src\assets\samples\7fffd3daccce43008a92674b0e265cf1.jpg" alt="Design 3" className="object-cover w-full h-full rounded-lg" />
+          </div>
         </div>
 
         {/* Right Section - Text, Color Pickers, Dropdown, and Buttons */}
@@ -39,7 +41,6 @@ const ViewDesigns = () => {
             </button>
             <button 
               className="bg-blue-800 text-white px-6 py-2 rounded-lg hover:bg-violet-950 transition"
-            //   onClick={}
             >
               Download
             </button>
